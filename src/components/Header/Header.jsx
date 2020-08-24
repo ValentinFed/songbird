2528e3c1df447b47b23f99580d609aa886001c51
Valentin <val.fediuk@gmail.com>
2020-08-24 21:52:07 +0200

    feat: add structure and style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,32 @@ import React, { useContext } from 'react';
 import classes from './Header.module.scss';
 import { MainContext } from '../../context/context';
 
+const titleQuestions = [
+  'Разминка',
+  'Воробьиные',
+  'Лесные птицы',
+  'Певчие птицы',
+  'Хищные птицы',
+  'Морские птицы',
+];
+
 const Header = () => {
   const headerContext = useContext(MainContext);
 
   return (
     <div className={classes.Header}>
-      <p>Logo</p>
-      <p>{headerContext.scope}</p>
+      <div className={classes.header__top}>
+        <h1 className={classes.logo}>SongBird</h1>
+        <p className={classes.scope}>
+          Score:
+          <span>{headerContext.scope}</span>
+        </p>
+      </div>
+
+      <ul className={classes.title__items}>
+        { titleQuestions.map((item) => <li className={classes.title__item}>{item}</li>)}
+      </ul>
+
     </div>
   );
 };
