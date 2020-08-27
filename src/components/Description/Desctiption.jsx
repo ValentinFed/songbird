0edb126c70347247e11a26706f3816f516db0270
Valentin <val.fediuk@gmail.com>
2020-08-27 15:25:11 +0200

    fix:

--- src/components/Description/Desctiption.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import classes from './Description.module.scss';
-
-const Description = () => <p className={classes.Description}>Description</p>;
-
-export default Description;
