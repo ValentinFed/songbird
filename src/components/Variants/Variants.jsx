68a4aa51d1f1f0abd769c9e5e866d0112cf8a5ba
Valentin <val.fediuk@gmail.com>
2020-08-28 10:28:33 +0200

    feat: dispalay variants from active question

--- src/components/Variants/Variants.jsx
+++ src/components/Variants/Variants.jsx
@@ -3,8 +3,9 @@ import classes from './Variants.module.scss';
 import { MainContext } from '../../context/context';
 
 const Variants = () => {
-  const variats = useContext(MainContext);
-  const data = variats.data[0];
+  const variatsContext = useContext(MainContext);
+  const { activeQuestion } = variatsContext;
+  const data = variatsContext.data[activeQuestion];
 
   return (
     <div className={classes.Variants}>
