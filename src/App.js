3f46f1a20574c666d467f71e5a09ebe9b0d55492
Valentin <val.fediuk@gmail.com>
2020-08-30 12:03:43 +0200

    refactor: change structure element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import classes from './App.module.scss';
-import Description from './components/Description/Description';
-import Header from './components/Header/Header';
-import Question from './components/Question/Question';
-import Variants from './components/Variants/Variants';
-import NextButton from './components/NextButton/NextButton';
 import MainProvider from './context/context';
+import Switcher from './components/Switcher/Switcher';
 
 function App() {
   return (
     <div className={classes.App}>
       <MainProvider>
-        <Header />
-        <Question />
-        <NextButton />
-        <div className={classes.answer}>
-          <Variants />
-          <Description />
-        </div>
+        <Switcher />
       </MainProvider>
     </div>
   );
